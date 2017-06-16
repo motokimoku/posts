16c30bb9128882cf504ec917609b69393f8bd0f7
motoki <mtk8vv614@gmail.com>
2017-06-16 04:13:44 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+ before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.all.page(params[:page])
   end
@@ -14,7 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    if user.save
+    if @user.save
       flash[:success] = 'ユーザを登録しました'
       redirect_to @user
     else
